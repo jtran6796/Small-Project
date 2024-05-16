06b282956b6215c92893b62f97b48c3fc010cf0f
ckrick04 <ckr20004@gmail.com>
2024-05-15 21:20:21 -0400

    Adapted other APIs to return JSON

--- api/session.php
+++ api/session.php
@@ -1,10 +1,29 @@
 <?php 
     session_start();
 
+    header("Content-Type: application/json");
+
+    function compose_response($sessionActive, $user, $uid, $rescode){
+        $res = [
+            "sessionActive" => $sessionActive,
+            "username" => $user,
+            "userid" => $uid,
+            "response_code" => $rescode
+        ];
+        return $res;
+    }
+
+    // If a user is logged in this field will be set
     if (!isset($_SESSION["username"])){
-        echo "";
+        echo json_encode(compose_response(
+            false, NULL, NULL, 404
+        ));
+        http_response_code(404);
     }
     else{
-        echo "Logged in as {$_SESSION["username"]}";
+        echo json_encode(compose_response(
+            true, $_SESSION["username"], $_SESSION["userid"], 200
+        ));
+        http_response_code(200);
     }
 ?>
